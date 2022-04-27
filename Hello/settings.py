205ea417885f9b3eb7c6c52f46422b7938004fc5
Vishesh <visheshvishwakarma2700@gmail.com>
2022-04-27 13:44:13 +0530

    ok

--- Hello/settings.py
+++ Hello/settings.py
@@ -52,6 +52,7 @@ MIDDLEWARE = [
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware'
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
