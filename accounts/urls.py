5791be33a7150526c19c55edc532087126850fd7
Vishesh <visheshvishwakarma2700@gmail.com>
2022-04-29 16:16:12 +0530

    swagger drf added

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,6 +4,26 @@ from knox import views as knox_views
 from .views import ChangePasswordView
 from django.urls import path, include
 
+
+
+from rest_framework import permissions             # for swagger
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Snippets API",
+      default_version='v1',
+      description="Test description",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@snippets.local"),
+      license=openapi.License(name="BSD License"),
+   )
+)
+
+
+
 urlpatterns = [
     path('api/register/', RegisterAPI.as_view(), name='register'),
      path('api/login/', LoginAPI.as_view(), name='login'),
@@ -12,5 +32,9 @@ urlpatterns = [
     path('api/change-password/', ChangePasswordView.as_view(), name='change-password'),
     path('api/password_reset/', include('django_rest_passwordreset.urls', namespace='password_reset')),
     
+
+    path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
 
