e4ec87e2bd1de74ed1188e92ceab869b402a740e
Vishesh <visheshvishwakarma2700@gmail.com>
2022-04-29 21:23:37 +0530

    make it better

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,8 @@
-from .views import RegisterAPI,LoginAPI
-from django.urls import path
+from .views import RegisterAPI
 from knox import views as knox_views
 from .views import ChangePasswordView
 from django.urls import path, include
-
+from .views import LoginAPI
 
 
 from rest_framework import permissions             # for swagger
@@ -14,11 +13,11 @@ from drf_yasg import openapi
 schema_view = get_schema_view(
    openapi.Info(
       title="MySite API",
-      default_version='v1',
+      default_version='v5',
       description="USER REGISTRATION API",
       terms_of_service="https://www.google.com/policies/terms/",
       contact=openapi.Contact(email="contact@snippets.local"),
-      license=openapi.License(name="BSD License"),
+      license=openapi.License(name="my license"),
    )
 )
 
@@ -26,13 +25,11 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('api/register/', RegisterAPI.as_view(), name='register'),
-     path('api/login/', LoginAPI.as_view(), name='login'),
-    path('api/logout/', knox_views.LogoutView.as_view(), name='logout'),
-    path('api/logoutall/', knox_views.LogoutAllView.as_view(), name='logoutall'),
+    path('api/login/', LoginAPI.as_view(), name='login'),
+    path('api/logout/',knox_views.LogoutView.as_view(), name='logout'),
+    path('api/logoutall/',knox_views.LogoutAllView.as_view(), name='logoutall'),
     path('api/change-password/', ChangePasswordView.as_view(), name='change-password'),
-    path('api/password_reset/', include('django_rest_passwordreset.urls', namespace='password_reset')),
-    
-
+    path('api/password_reset/', include('django_rest_passwordreset.urls', namespace='password_reset')),    
     path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
