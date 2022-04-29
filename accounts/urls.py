ff0560d486a58c3270398ae1f05893e1fc225cc6
Vishesh <visheshvishwakarma2700@gmail.com>
2022-04-29 16:19:09 +0530

    ok

--- accounts/urls.py
+++ accounts/urls.py
@@ -13,9 +13,9 @@ from drf_yasg import openapi
 
 schema_view = get_schema_view(
    openapi.Info(
-      title="Snippets API",
+      title="MySite API",
       default_version='v1',
-      description="Test description",
+      description="USER REGISTRATION API",
       terms_of_service="https://www.google.com/policies/terms/",
       contact=openapi.Contact(email="contact@snippets.local"),
       license=openapi.License(name="BSD License"),
